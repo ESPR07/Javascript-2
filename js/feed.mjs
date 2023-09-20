887b054619894993a60f0c7583ba8932e47831db
ESPR07 <111272036+ESPR07@users.noreply.github.com>
2023-09-20 14:23:11 +0200

    Update feed.mjs

--- js/feed.mjs
+++ js/feed.mjs
@@ -9,73 +9,78 @@ const searchButton = document.querySelector(".search-button");
 
 function postCardTemplate(json) {
   const searchValue = document.querySelector(".searchbar").value;
-  const filteredArray = json.filter((json) => json.created.includes(dateSelector.value));
-  const searchedArray = filteredArray.filter((json) => json.author.name.toLowerCase().includes(searchValue.toLowerCase()) || json.body && json.body.toLowerCase().includes(searchValue.toLowerCase()));
+  const filteredArray = json.filter((json) =>
+    json.created.includes(dateSelector.value)
+  );
+  const searchedArray = filteredArray.filter(
+    (json) =>
+      json.author.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+      (json.body && json.body.toLowerCase().includes(searchValue.toLowerCase()))
+  );
+
   searchedArray.forEach(({ body, created, author }) => {
-      const date = new Date(created);
-      const formatDate = (date) => {
-        return (
-          date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear()
-        );
-      };
+    const date = new Date(created);
+    const formatDate = (date) => {
+      return date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear();
+    };
 
-      const cardContainer = document.createElement("div");
-      cardContainer.classList.add("post");
-      cardSection.append(cardContainer);
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("post");
+    cardSection.append(cardContainer);
 
-      const postInfo = document.createElement("div");
-      postInfo.classList.add("post-info");
-      postInfo.classList.add("d-flex");
-      postInfo.classList.add("justify-content-between");
-      cardContainer.append(postInfo);
+    const postInfo = document.createElement("div");
+    postInfo.classList.add("post-info");
+    postInfo.classList.add("d-flex");
+    postInfo.classList.add("justify-content-between");
+    cardContainer.append(postInfo);
 
-      const postUsername = document.createElement("a");
-      postUsername.href = "/profile.html";
-      postUsername.classList.add("user-name");
-      postUsername.innerText = author.name;
-      postInfo.append(postUsername);
+    const postUsername = document.createElement("a");
+    postUsername.href = "/profile.html";
+    postUsername.classList.add("user-name");
+    postUsername.innerText = author.name;
+    postInfo.append(postUsername);
 
-      const postTimePosted = document.createElement("p");
-      postTimePosted.classList.add("time-posted");
-      postTimePosted.classList.add("mb-0");
-      postTimePosted.innerText = formatDate(date);
-      postInfo.append(postTimePosted);
+    const postTimePosted = document.createElement("p");
+    postTimePosted.classList.add("time-posted");
+    postTimePosted.classList.add("mb-0");
+    postTimePosted.innerText = formatDate(date);
+    postInfo.append(postTimePosted);
 
-      const postContent = document.createElement("div");
-      postContent.classList.add("post-content");
-      cardContainer.append(postContent);
+    const postContent = document.createElement("div");
+    postContent.classList.add("post-content");
+    cardContainer.append(postContent);
 
-      const userPostImage = document.createElement("a");
-      userPostImage.href = "/profile.html";
-      userPostImage.classList.add("user-post-image");
-      postContent.append(userPostImage);
+    const userPostImage = document.createElement("a");
+    userPostImage.href = "/profile.html";
+    userPostImage.classList.add("user-post-image");
+    postContent.append(userPostImage);
 
-      if (author.avatar === "" || author.avatar === null) {
-      } else {
-        userPostImage.style.backgroundImage = `url(${author.avatar})`;
-      }
+    if (author.avatar === "" || author.avatar === null) {
+    } else {
+      userPostImage.style.backgroundImage = `url(${author.avatar})`;
+    }
 
-      const postContentText = document.createElement("p");
-      postContentText.classList.add("post-content-text");
-      postContentText.innerText = body;
-      postContent.append(postContentText);
+    const postContentText = document.createElement("p");
+    postContentText.classList.add("post-content-text");
+    postContentText.innerText = body;
+    postContent.append(postContentText);
 
-      const postInteractions = document.createElement("div");
-      postInteractions.classList.add("post-interactions");
-      postInteractions.classList.add("d-flex");
-      postInteractions.classList.add("justify-content-end");
-      postContent.append(postInteractions);
+    const postInteractions = document.createElement("div");
+    postInteractions.classList.add("post-interactions");
+    postInteractions.classList.add("d-flex");
+    postInteractions.classList.add("justify-content-end");
+    postContent.append(postInteractions);
 
-      const postEdit = document.createElement("a");
-      postEdit.href = "#";
-      postEdit.innerText = "Edit";
-      postInteractions.append(postEdit);
+    const postEdit = document.createElement("a");
+    postEdit.href = "#";
+    postEdit.innerText = "Edit";
+    postInteractions.append(postEdit);
 
-      const postDelete = document.createElement("a");
-      postDelete.href = "#";
-      postDelete.classList.add("ms-3");
-      postDelete.innerText = "Delete";
-      postInteractions.append(postDelete);
+    const postDelete = document.createElement("a");
+    postDelete.href = "#";
+    postDelete.classList.add("ms-3");
+    postDelete.innerText = "Delete";
+    postInteractions.append(postDelete);
   });
 }
 
