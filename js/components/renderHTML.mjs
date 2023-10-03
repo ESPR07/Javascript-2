7d57843632ad85427cb495f2ad9dbea0339ced72
ESPR07 <111272036+ESPR07@users.noreply.github.com>
2023-10-03 10:36:04 +0200

    Added single post page

--- js/components/renderHTML.mjs
+++ js/components/renderHTML.mjs
@@ -7,7 +7,7 @@ const cardSection = document.querySelector(".posts-container");
  * Function that renders the posts based on API response as well as filtering if a filter or searchword has been passed in.
  * @param {object} json The JSON Response from API Call
  */
-export function postCardTemplate(json) { //se om man kan modulere koden!
+export function postCardTemplate(json) {
   const searchValue = document.querySelector(".searchbar").value;
   const loggedEmail = localStorage.getItem("email");
   const filteredArray = json.filter((json) =>
@@ -48,8 +48,9 @@ export function postCardTemplate(json) { //se om man kan modulere koden!
     postTimePosted.innerText = formatDate(date);
     postInfo.append(postTimePosted);
 
-    const postContent = document.createElement("div");
+    const postContent = document.createElement("a");
     postContent.classList.add("post-content");
+    postContent.href = `/singlePost.html?id=${id}`;
     cardContainer.append(postContent);
 
     const userPostImage = document.createElement("a");
@@ -73,7 +74,7 @@ export function postCardTemplate(json) { //se om man kan modulere koden!
     postInteractions.classList.add("justify-content-end");
     postContent.append(postInteractions);
 
-    if(author.email === loggedEmail) {
+    if (author.email === loggedEmail) {
       const postEdit = document.createElement("a");
       postEdit.href = "#";
       postEdit.innerText = "Edit";
@@ -84,21 +85,21 @@ export function postCardTemplate(json) { //se om man kan modulere koden!
         postContentText.innerText = "";
 
         const editInput = document.createElement("textarea");
-        editInput.rows = "3"
-        editInput.classList.add("post-content-text")
-        editInput.style.width = "80%"
+        editInput.rows = "3";
+        editInput.classList.add("post-content-text");
+        editInput.style.width = "80%";
         editInput.value = body;
         postContentText.append(editInput);
 
         const editSubmit = document.createElement("a");
-        editSubmit.href = "#"
+        editSubmit.href = "#";
         editSubmit.innerText = "Update";
         postInteractions.append(editSubmit);
         editSubmit.addEventListener("click", () => {
-          updatePost(editInput.value, id)
-        })
+          updatePost(editInput.value, id);
+        });
       });
-  
+
       const postDelete = document.createElement("a");
       postDelete.href = "#";
       postDelete.classList.add("ms-3");
@@ -106,7 +107,103 @@ export function postCardTemplate(json) { //se om man kan modulere koden!
       postInteractions.append(postDelete);
       postDelete.addEventListener("click", () => {
         deletePost(id);
-      })
+      });
     }
   });
+}
+
+/**
+ * Renders a single post fetched by using ID
+ * @param {object} json 
+ */
+export function singleCardTemplate(json) {
+  const loggedEmail = localStorage.getItem("email");
+  const date = new Date(json.created);
+  const formatDate = (date) => {
+    return date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear();
+  };
+
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("post");
+  cardSection.append(cardContainer);
+
+  const postInfo = document.createElement("div");
+  postInfo.classList.add("post-info");
+  postInfo.classList.add("d-flex");
+  postInfo.classList.add("justify-content-between");
+  cardContainer.append(postInfo);
+
+  const postUsername = document.createElement("a");
+  postUsername.href = "/profile.html";
+  postUsername.classList.add("user-name");
+  postUsername.classList.add("fs-4");
+  postUsername.innerText = json.author.name;
+  postInfo.append(postUsername);
+
+  const postTimePosted = document.createElement("p");
+  postTimePosted.classList.add("time-posted");
+  postTimePosted.classList.add("mb-0");
+  postTimePosted.innerText = formatDate(date);
+  postInfo.append(postTimePosted);
+
+  const postContent = document.createElement("div");
+  postContent.classList.add("post-content");
+  cardContainer.append(postContent);
+
+  const userPostImage = document.createElement("a");
+  userPostImage.href = "/profile.html";
+  userPostImage.classList.add("user-post-image");
+  postContent.append(userPostImage);
+
+  if (json.author.avatar === "" || json.author.avatar === null) {
+  } else {
+    userPostImage.style.backgroundImage = `url(${json.author.avatar})`;
+  }
+
+  const postContentText = document.createElement("p");
+  postContentText.classList.add("post-content-text");
+  postContentText.innerText = json.body;
+  postContent.append(postContentText);
+
+  const postInteractions = document.createElement("div");
+  postInteractions.classList.add("post-interactions");
+  postInteractions.classList.add("d-flex");
+  postInteractions.classList.add("justify-content-end");
+  postContent.append(postInteractions);
+
+  if (json.author.email === loggedEmail) {
+    const postEdit = document.createElement("a");
+    postEdit.href = "#";
+    postEdit.innerText = "Edit";
+    postInteractions.append(postEdit);
+    postEdit.addEventListener("click", () => {
+      postEdit.style.display = "none";
+      postDelete.style.display = "none";
+      postContentText.innerText = "";
+
+      const editInput = document.createElement("textarea");
+      editInput.rows = "3";
+      editInput.classList.add("post-content-text");
+      editInput.style.width = "80%";
+      editInput.value = json.body;
+      postContentText.append(editInput);
+
+      const editSubmit = document.createElement("a");
+      editSubmit.href = "#";
+      editSubmit.innerText = "Update";
+      postInteractions.append(editSubmit);
+      editSubmit.addEventListener("click", () => {
+        updatePost(editInput.value, json.id);
+      });
+    });
+
+    const postDelete = document.createElement("a");
+    postDelete.href = "#";
+    postDelete.classList.add("ms-3");
+    postDelete.innerText = "Delete";
+    postInteractions.append(postDelete);
+    postDelete.addEventListener("click", () => {
+      deletePost(json.id);
+    });
+  }
 }
